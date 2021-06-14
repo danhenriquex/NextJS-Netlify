124b31c3ab3bd9582169c9f9455bf547b4b6676a
danilohenrique <danilosantana@mat.ci.ufpb.br>
2021-06-14 00:13:45 -0300

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.home}>
+       <h2>Welcome to Gaming Vibes</h2>
+      <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deleniti rem aspernatur odit hic autem neque repellat alias? Debitis veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor id. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deleniti rem aspernatur odit hic autem neque repellat alias? Debitis veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor id. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deleniti rem aspernatur odit hic autem neque repellat alias? Debitis veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor id.</p>
+      </div>
+    </div>
+  )
+}
